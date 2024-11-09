00eac01487ad618263d70121796961400df00f32
gergogyulai <gergoo.gyulai@icloud.com>
2024-11-09 01:27:59 +0100

    refactor: remove outdated next.config.mjs and update in favor of next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "raw.githubusercontent.com",
-        pathname: "/**",
+        pathname: "/yetweets/vault/**",
       },
     ],
   }
